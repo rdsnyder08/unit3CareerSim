0b717b93c538551eda45e86e4bff51f7feb13234
rdsnyder08 <rdsnyder08@gmail.com>
2023-08-02 13:45:30 -0400

    added a register component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import {Route,Routes} from 'react-router-dom'
 import PostViewUnauth from './components/PostViewUnauth'
+import Register from './components/Register'
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<PostViewUnauth />} />
+        <Route path='/' element={<Register />} />
+        <Route path='/posts' element={<PostViewUnauth />} />
       </Routes>
     </>
   )
---
+++ vite-project/src/components/Register.jsx
@@ -0,0 +1,64 @@
+import {useState,useEffect} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+
+const COHORT_NAME = '2306-FTB-WEB-FT'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+
+export default function Register ({setToken}) {
+
+    const [username,setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        try {
+            const response = await fetch(`${BASE_URL}/users/register`,
+                {
+                    method:'POST',
+                    headers: {
+                        "Content-Type":"application/json"
+                    },
+                    body: JSON.stringify({
+                        user: {
+                            username: username,
+                            password: password
+                }})
+                }
+            
+            
+            )
+
+            const data = await response.json()
+            console.log(data)
+
+            setToken(data.token)
+
+            setUsername('')
+            setPassword('')
+
+
+        } catch (err) {
+            setError(err)
+        }
+    }
+
+    return <>
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Username:
+                <input value={username} onChange={e=>setUsername(e.target.value)} />
+            </label>
+            <label>Password:
+                <input value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type='submit'>Register</button>
+        </form>
+        
+        <Link to='/posts'>Or view Posts without registering</Link>
+
+    </>
+
+
+}
